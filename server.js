56a0ceee0b56d8a3490dbf26a9498d9399d5adfa
ERUD1T3 <josiasmoukpe@gmail.com>
2019-09-14 15:33:00 -0400

    converted to nodejs

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict'
+// Code for server side
+const express = require('express')
+const app = express()
+
+const morgan = require('morgan')
+const layouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const server = require('http').Server(app)
+
+app.use(morgan('dev')) // logging requests to server
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views/')
+app.set('layout', 'main') // not a file path; does a lookpu
+
+
+app.use(layouts)
+app.use(express.static(__dirname + '/public')) // serving frontend file; index.html is starting point
+app.use(express.json()) // middleware to read json
+app.use(bodyParser.urlencoded({
+    limit: '5mb',
+    extended: false
+}))
+
+
+
+server.listen(8080, (error) => {
+    console.log((error) ? error : `***SeaIRL***\nServer up on ${8080}`)
+})

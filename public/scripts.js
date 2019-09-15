f93af8308fa669256205304716f893a729398d97
ERUD1T3 <josiasmoukpe@gmail.com>
2019-09-15 09:56:16 -0400

    update

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,10 @@
 // file for public scripts
+
+
+let inputs = document.getElementsByName('input')
+
+for (let input of inputs) {
+    input.onmouseover = function () {
+        input.checked = true
+    }
+}

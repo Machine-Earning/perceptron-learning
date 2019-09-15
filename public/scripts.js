05147af88b3e0eacd1fcc8c7075d54c5d4b3bf1a
ERUD1T3 <josiasmoukpe@gmail.com>
2019-09-14 23:16:52 -0400

    clean up the html

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,7 @@
 // file for public scripts
+
+$(document).ready(function () {
+    $('.hover').mouseover(function () {
+        $(this).attr('checked', 'checked');
+    });
+});
--- sources/main.js
+++ sources/main.js
@@ -3,12 +3,18 @@ const router = require('express').Router()
 const Data = require('./data')
 const Perceptron = require('./perceptron')
 
-
+const DATASET = []
 
 
 router.get('/', (req, res) => {
     res.render('layout')
     console.log('Enter Perceptron')
+})
+
+router.post('/', (req, res) => {
+    DATASET.push(new Data(req.body.input, req.body.target))
+    console.log('Data added to Dataset')
+    res.redirect('/')
 })
 
 
